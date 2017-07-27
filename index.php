0cb5afa906d34e5e690304f8a0af4aa6cd5ee27e
mafloria <margareth.florian@gmail.com>
2017-07-27 14:06:28 -0500

    espacios

--- index.php
+++ index.php
@@ -84,12 +84,12 @@
                 </section>         
     
                 <div class="gnc-biogas-landscape landscape-section">
-                    <?php require_once("sections/biogas.php")?>                
+                    <?php require_once("sections/biogas.php")?>
     
                 </div> <!-- end gnc-biogas-landscape -->
                 
                 <div class="gnc-yacimiento-landscape landscape-section">
-                    <?php require_once("sections/yacimientogas.php")?>                
+                    <?php require_once("sections/yacimientogas.php")?>
     
                 </div> <!-- end gnc-yacimiento-landscape -->
             </div><!-- end front scroll -->

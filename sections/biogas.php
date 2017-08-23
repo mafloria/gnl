486a9c675afc35b79d5f55edd6bd06550476b105
Alvaro Rojas Tafur <arojastafur@gmail.com>
2017-08-23 17:02:51 -0500

    ruedas y camion

--- sections/biogas.php
+++ sections/biogas.php
@@ -77,6 +77,14 @@
                      
                     <!-- CAMION -->
                     <div id="truck-biogas" class="camion firstTruck-biogas-stop"></div>
+
+                    <div id="" class="camion-animated truck-fixed">
+                        <div class="rueda rueda1"></div>
+                        <div class="rueda rueda2"></div>
+                        <div class="rueda rueda3"></div>
+                        <div class="rueda rueda4"></div>
+                        <div class="rueda rueda5"></div>
+                    </div>
                     <!--CAMION -->
                      
                      <div class="group-animated animatedParent"  data-appear-left-offset='-3600'  data-sequence='300'>

cac5c91ff76971db8296e266dacd1309f1d53fa3
Margaret <margareth.florian@gmail.com>
2017-08-26 00:07:35 -0500

    sin viewport

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Galileo technologies</title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        
 
         <link rel="stylesheet" href="assets/css/normalize.css">        
         <link rel="stylesheet" href="assets/css/main.css">
